8161ffffc12ca9afa94e3ff3535c2bab730981f4
Antoine LABORDERIE <alaborderie@hellowork.com>
2025-04-13 14:52:54 +0200

    :lipstick: ran cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use askama::Template;
-use axum::{
-    response::Html,
-    routing::get,
-    Router,
-};
+use axum::{response::Html, routing::get, Router};
 use tower_http::services::ServeDir;
 
 #[tokio::main]
@@ -16,10 +12,11 @@ async fn main() {
 
     // Run on localhost:3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("Server running at http://{}", listener.local_addr().unwrap());
-    axum::serve(listener, app)
-        .await
-        .unwrap();
+    println!(
+        "Server running at http://{}",
+        listener.local_addr().unwrap()
+    );
+    axum::serve(listener, app).await.unwrap();
 }
 
 // Templates
